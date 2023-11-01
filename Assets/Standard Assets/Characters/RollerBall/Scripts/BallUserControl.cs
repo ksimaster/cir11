298648a893d156a7d88cd3eb3cda8a053adb56b6
ksimaster <ksimasterlord@gmail.com>
2023-11-01 18:33:45 +0300

    21 + fix errors

--- Assets/Standard Assets/Characters/RollerBall/Scripts/BallUserControl.cs
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/BallUserControl.cs
@@ -1,6 +1,6 @@
 using System;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
+//using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Vehicles.Ball
 {
@@ -40,21 +40,21 @@ namespace UnityStandardAssets.Vehicles.Ball
         {
             // Get the axis and jump input.
 
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
-            jump = CrossPlatformInputManager.GetButton("Jump");
+           // float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            //float v = CrossPlatformInputManager.GetAxis("Vertical");
+           // jump = CrossPlatformInputManager.GetButton("Jump");
 
             // calculate move direction
             if (cam != null)
             {
                 // calculate camera relative direction to move:
                 camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
-                move = (v*camForward + h*cam.right).normalized;
+               // move = (v*camForward + h*cam.right).normalized;
             }
             else
             {
                 // we use world-relative directions in the case of no main camera
-                move = (v*Vector3.forward + h*Vector3.right).normalized;
+               // move = (v*Vector3.forward + h*Vector3.right).normalized;
             }
         }
 
